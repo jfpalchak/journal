712364502dc1b0bd94636692a0b09c3c003b814c
Joey Palchak <joey.palchak@gmail.com>
2023-08-28 15:15:44 -0700

    Pass Test 1 for wordCounter().

--- src/entry.js
+++ src/entry.js
@@ -4,3 +4,11 @@ function Entry(title, journalText) {
   this.journalText = journalText;
 }
 
+Entry.prototype.wordCounter = function () {
+  const textArr = this.journalText.split(" ").length;
+  return textArr;
+}
+
+const newEntry = new Entry("Mondays suck.", "Still Mondays suck.");
+
+newEntry.wordCounter();
