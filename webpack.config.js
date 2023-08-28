edc748cbfc3a0666a336c748224301a9c2f484a6
Joey Palchak <joey.palchak@gmail.com>
2023-08-28 14:30:57 -0700

    Install & configure plugins for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -7,8 +10,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {                 // new line
+    contentBase: './dist'      // new line
+  },        
+  devtool: 'eval-source-map',
   // The plugins key below this line is also new!
   plugins: [
+    new CleanWebpackPlugin({ // new line
+			verbose: true // new line - terminal prints when files are removed
+		}),
+    new ESLintPlugin(),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
