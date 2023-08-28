c6be4b9472eb93eeca77c9799de29f341817d26b
Joey Palchak <joey.palchak@gmail.com>
2023-08-28 16:19:01 -0700

    Refactor vowelCounter and consonantCounter.

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,5 @@
 
-function Entry(title, journalText) {
+export default function Entry(title, journalText) {
   this.title = title;
   this.journalText = journalText;
 }
@@ -8,6 +8,27 @@ Entry.prototype.wordCounter = function () {
   const textArr = this.journalText.split(" ").length;
   return textArr;
 }
+
+// returns count of all vowels in journal text
+Entry.prototype.consonantCounter = function() {
+    const textArray = this.journalText.split('');
+  
+    let vowelArray = ["a", "e", "i", "o", "u"];
+    let vowels = 0;
+    let consonants = 0;
+
+    textArray.forEach(function(letter) {
+        if (!vowelArray.includes(letter.toLowerCase())) {
+            consonants++;
+        }
+        else {
+            vowels++;
+        }
+    });  
+  
+    return [consonants, vowels];
+  }
+
 
 const newEntry = new Entry("Mondays suck.", "Still Mondays suck.");
 
